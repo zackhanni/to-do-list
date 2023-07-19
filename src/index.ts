b2d2a917fc42fe5318cc8551331d17f6195ddb7e
Zack Hanni <zack.hannI@gmail.com>
2023-07-19 14:39:13 -0400

    make li from text submitted in form
    assigned an id, completed checkbox, and date to the task

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { v4 as uuidV4 } from "uuid"
+
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector("#new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if (input?.value == "" || input?.value == null) return 
+  
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  addListItem(newTask)
+
+})
+
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
